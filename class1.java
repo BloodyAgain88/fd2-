74aeeb7c5aaaf80e5c7365d7b47f98da64a024ea
Srajan Narsingh <srajannarsingh42@gmail.com>
2025-08-11 11:07:23 +0530

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,9 @@
+public class class1 {
+    public static void main (String[] var0){
+        int fd=1;//local variable
+        // local variables can start with a letter, underscore, or dollar sign
+        int _var = 2;
+        int $var = 3;
+        System.out.println("fd: " + fd + ", _var: " + _var + ", $var: " + $var);
+    }
+}
